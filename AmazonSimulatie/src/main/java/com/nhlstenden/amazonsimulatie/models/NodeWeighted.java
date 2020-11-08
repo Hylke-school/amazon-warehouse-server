822efe9cb12f6e83f250656bea9d4553656e7d49
Jordy Walter <youngstrike@hotmail.com>
2020-11-08 11:43:57 +0100

    Added coordinates to the nodes.

--- AmazonSimulatie/src/main/java/com/nhlstenden/amazonsimulatie/models/NodeWeighted.java
+++ AmazonSimulatie/src/main/java/com/nhlstenden/amazonsimulatie/models/NodeWeighted.java
@@ -5,17 +5,29 @@ import java.util.LinkedList;
 public class NodeWeighted {
 
     private int n;
+    private int x;
+    private int y;
     private String name;
     private boolean visited;
     private boolean occupied;
     private LinkedList<EdgeWeighted> edges;
 
-    public NodeWeighted(int n, String name){
+    public NodeWeighted(int n, String name,int x,int y){
         this.n = n;
+        this.x = x;
+        this.y = y;
         this.name = name;
         visited = false;
         occupied = false;
         edges = new LinkedList<>();
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
     }
 
     public LinkedList<EdgeWeighted> getEdges(){
